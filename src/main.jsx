d127ebe2505e0dfc8849279668310336b230d34e
ninnikukun <ek17.fcsj@gmail.com>
2026-07-05 17:07:31 +0900

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,10 @@ createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+
+// PWA: ホーム画面追加・オフライン起動のためのservice worker登録
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {});
+  });
+}
